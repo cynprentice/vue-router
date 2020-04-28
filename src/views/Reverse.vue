74567efce3b49252d8f1395b9b49ebd453f04ecd
cynprentice <cprentice@seattleu.edu>
2020-04-28 16:55:13 -0700

    first commit

---
+++ src/views/Reverse.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="reverse">
+    <h1>This is a reverse page</h1>
+    <h2>Please enter a string to reverse!</h2>
+    <label for="input-string"></label> 
+      <input type="text" id="input-string" v-model.lazy="message">
+
+  <p>{{reveredMessage}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name:"Reverse",
+  data(){
+    return {
+      message:''
+    }
+  },
+  computed: {
+    reveredMessage() {
+      return this.message.split('').reverse().join('');
+    }
+  }
+}
+</script>
